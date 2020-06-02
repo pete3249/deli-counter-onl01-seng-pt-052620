65d4c628c79d081c6141970f45748fdf8a5e1a2b
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-02 19:13:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.length == 0
-    katz_deli << "#{name}"
+    puts "Welcome, #{name}. You are number 1 inline."
   else
     puts "Hi"
   end 
