b3c4f6950c3a1331f383530d02b70aea770a6814
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-02 18:52:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,14 @@
 require 'pry'
 
 def line(customer_order)
+  labeled_customer_names = []
   if customer_order[0] == nil
     puts "The line is currently empty."
   else 
+    customer_order.each_with_index do |name, index|
+      binding.pry
+    labeled_customer_names << "[index + 1]. name"
     puts "The line is currently:" + customer_order.join(" ")
+    end 
   end 
 end
